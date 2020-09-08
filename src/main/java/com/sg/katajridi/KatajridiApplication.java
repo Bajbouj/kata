c11731386bde1d584480d226a37513b5a9cadcdb
jridi <mohamed.jridi@esprit.tn>
2020-09-08 14:20:53 +0200

    init repository

---
+++ src/main/java/com/sg/katajridi/KatajridiApplication.java
@@ -0,0 +1,13 @@
+package com.sg.katajridi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatajridiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KatajridiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sg/katajridi/KatajridiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.katajridi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatajridiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
